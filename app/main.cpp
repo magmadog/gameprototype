d5bcdf863b63a56efde3b8e7ebb1a3f8eb127b29
magmadog <36441904+magmadog@users.noreply.github.com>
2018-10-23 22:30:42 +0300

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -21,31 +21,11 @@ int main(int argc, char *argv[])
         qmlRegisterType<Enemy>("com.Enemy",1,0,"Enemy");
         qmlRegisterType<herocontrol>("com.Hero",1,0,"Herocontrol");
 
-    engine.load(QUrl(QStringLiteral("qrc:/gameprototype.qmlproject")));
+    engine.load(QUrl(QStringLiteral("qrc:/gameprototype.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
     return app.exec();
 
 }
-/*
-int main(int argc, char *argv[])
-{
-  QGuiApplication app(argc, argv);
-  QQuickView view;
-  view.setSource(QUrl("qrc:///main.qml"));
-  view.show();
-  QQuickItem* child;
-  QQmlApplicationEngine engine;
-  engine.load(QUrl(QStringLiteral("qrc:///gameprototype.qml")));
-  QObject *rootObject = engine.rootObjects().first();
-  QQuickItem *qmlObject = rootObject->findChild<QQuickItem*>("grid")->findChild<QQuickItem*>("repeter");
-  QMetaObject::invokeMethod(qmlObject,"itemAt",Qt::DirectConnection,   Q_RETURN_ARG (QQuickItem*,child), Q_ARG(int,32));
-  child=child->findChild<QQuickItem*>("pleaseWork");
-  qDebug() << child->property("visible");
-  child->setProperty("visible","true");
-  qDebug() << child->property("visible");
-  return app.exec();
-}
-I used qDebug to verify the property changed
-*/
+
